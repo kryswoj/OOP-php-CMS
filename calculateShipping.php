30dedfe854601c6eb5836dd9e15359fb53fb73f2
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-03-15 17:18:53 +0100

    Simple calculateShipping logic

--- calculateShipping.php
+++ calculateShipping.php
@@ -3,6 +3,7 @@
 class Product {
     private $price;
     private $weight;
+    private $freeShipping;
 
     public function __construct($price, $weight)
     {
@@ -14,21 +15,64 @@ class Product {
     {
         return $this->weight;
     } 
+
+    public function setFreeShipping()
+    {
+        $this->freeShipping = true;
+    }
+
+    public function getFreeShipping()
+    {
+        return $this->freeShipping;
+    }
 }
 
 class Shipping {
     private $totalShipping;
+    private $products;
+    private $pricePerKilogram;
+    private $shippingProvider;
 
-    public function calculateTotalShipping($weight, $pricePerKilogram)
+    public function __construct($pricePerKilogram, $shippingProvider)
     {
-        return $weight * $pricePerKilogram;
+        $this->pricePerKilogram = $pricePerKilogram;
+        $this->shippingProvider = $shippingProvider;
+    }
+
+    public function addProducts(Product $product)
+    {
+        $this->products[] = $product;
+    }
+
+    public function calculateTotalShipping()
+    {
+        foreach ($this->products as $product) {
+            if(!$product->getFreeShipping()) {
+                $this->totalShipping += $product->getWeight() * $this->pricePerKilogram;
+            }
+        }
+    }
+
+    public function getTotalShippingPrice()
+    {
+
+        return $this->totalShipping;
     }
 }
 
 $product = new Product(5, 1);
-$shipping = new Shipping();
+$product2 = new Product(1, 2);
+$product3 = new Product(3, 5);
+$product3->setFreeShipping();
 
-$totalShippingPrice = $shipping->calculateTotalShipping($product->getWeight(), $pricePerKilogram);
+$shipping = new Shipping(5, '');
+$shipping->addProducts($product3);
+$shipping->addProducts($product2);
+$shipping->addProducts($product);
+
+$shipping->calculateTotalShipping();
 
 
-echo $product->getWeight();
+
+
+var_dump($shipping->getTotalShippingPrice());
